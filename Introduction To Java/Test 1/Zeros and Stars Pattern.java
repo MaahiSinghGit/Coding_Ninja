83f9c88a22fc86cc17fff2ff85149863d8e3fb01
Manoj Singh <86585545+MaahiSinghGit@users.noreply.github.com>
2022-02-11 21:54:55 +0530

    Add files via upload
    First Milestone Completed

---
+++ Introduction To Java/Test 1/Zeros and Stars Pattern.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Solution {
+
+	public static void main(String[] args) {
+
+		/*  Read input as specified in the question.
+    		 * Print output as specified in the question.
+    		 */
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        
+        while (i<=n){
+            
+            int j = 1;
+            while (j<=(2*n+1)){
+                
+                if (j==(((2*n)+2)/2)){
+                    System.out.print('*');
+                    j++;
+                }
+                
+                else if (j==i){
+                    System.out.print('*');
+                    j++;
+                }
+                
+                else if (j==((2*n+2)-i)){
+                    System.out.print('*');
+                    j++;
+                }
+                
+                else{
+                System.out.print(0);
+                j++;
+                }
+            }
+            System.out.println();
+            i++;
+            
+        }
+	}	
+
+
+}
