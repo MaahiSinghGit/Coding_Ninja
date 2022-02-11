64e7031b928b38639d4ae24c5dbb64f8e127a1a4
Manoj Singh <86585545+MaahiSinghGit@users.noreply.github.com>
2022-02-11 21:54:01 +0530

    Add files via upload

---
+++ Introduction To Java/Test 1/Number Star pattern 1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class runner {
+
+	public static void main(String[] args) {
+		// Write your code here
+         Scanner s = new Scanner(System.in);
+        
+        int n = s.nextInt();
+        
+        for (int i = 1; i<=n; i++){
+            int j = n;
+            while (j>=1){
+                
+                if (j==i){
+                    System.out.print("*");
+                    j--;
+                }
+                else{
+                System.out.print(j);
+                j--;
+                }
+            }
+            System.out.println();
+            
+            
+        }
+
+	}
+}
