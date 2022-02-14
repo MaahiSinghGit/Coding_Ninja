bb6e0a40b57d41d230da0c38bd0fe5907a5ce787
Manoj Singh <86585545+MaahiSinghGit@users.noreply.github.com>
2022-02-14 22:26:28 +0530

    Add files via upload

---
+++ Introduction To Java/Lecture 9 Arrays/Assignment/Pair Sum.java
@@ -0,0 +1,17 @@
+public class Solution {  
+
+    public static int pairSum(int arr[], int x) {
+    	//Your code goes here
+        int sum=0,temp=0,count=0;
+        for(int i=0;i<arr.length;i++)
+        { temp=arr[i];
+		   for(int j=i+1;j<arr.length;j++)
+           {
+               if(temp+arr[j]==x)
+               {
+                   count ++;
+               }
+           }
+        }return count;
+    }
+}
