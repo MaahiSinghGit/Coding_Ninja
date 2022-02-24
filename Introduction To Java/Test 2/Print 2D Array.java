24123714fc66d5b032f6b4f9bc167d5e745484b7
Manoj Singh <86585545+MaahiSinghGit@users.noreply.github.com>
2022-02-24 23:14:02 +0530

    Add files via upload

---
+++ Introduction To Java/Test 2/Print 2D Array.java
@@ -0,0 +1,18 @@
+public class solution {
+	public static void print2DArray(int input[][]) {
+		// Write your code here
+         int rows = input.length;
+        int cols = input[0].length;
+        
+        for(int i = 0; i<rows; i++){
+            for(int k = 0; k<rows-i; k++){
+            for(int j = 0; j<cols; j++){
+                System.out.print(input[i][j] + " ");
+            }
+            System.out.println();
+            }
+        }
+
+
+	}
+}
