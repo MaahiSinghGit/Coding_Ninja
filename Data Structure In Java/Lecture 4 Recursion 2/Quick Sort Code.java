e1653f19ce83b52e554792c7223328a24b4a322c
Manoj Singh <86585545+MaahiSinghGit@users.noreply.github.com>
2022-03-30 00:58:19 +0530

    Add files via upload

---
+++ Data Structure In Java/Lecture 4 Recursion 2/Quick Sort Code.java
@@ -0,0 +1,53 @@
+public class Solution {
+	
+	public static void quickSort(int[] arr) {
+		quickSort(arr,0,arr.length-1);
+	}
+	
+	public static void quickSort(int[] arr,int start,int end) {
+		
+		if(start>=end) {
+			return ;
+		}
+		int pivotPos=partition(arr,start,end);
+		quickSort(arr, start, pivotPos-1);
+		quickSort(arr, pivotPos+1, end);
+		
+	}
+	public static int partition(int[] arr,int start,int end) {
+		
+		int pivot=arr[start];
+		int count=0;
+		for(int i=start+1;i<=end;i++) {
+			if(arr[i] <= pivot) {
+				count++;
+			}
+		}
+		
+		int pivotPos=start+count;
+		int temp=arr[pivotPos];
+		arr[pivotPos]=arr[start];
+		arr[start]=temp;
+		
+		int i=start,j=end;
+		while(i < pivotPos && j> pivotPos) {
+			if(arr[i] <= pivot) {
+				i++;
+			}
+			else if(arr[j] > pivot) {
+				j--;
+			}
+			else {
+				int temp2=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp2;
+				i++;
+				j--;
+				
+			}
+		}
+		return pivotPos;
+		
+	}
+	
+}
