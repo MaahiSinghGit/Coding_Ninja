ce4de7581fa46ccdf8124973f504c736674c876a
Manoj Singh <86585545+MaahiSinghGit@users.noreply.github.com>
2022-05-23 23:06:01 +0530

    Add files via upload

---
+++ Data Structure In Java/Lecture 8 Linked List 1/Assignment/Print Reverse LinkedList.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+	public static void printReverse(LinkedListNode<Integer> root) {
+		//Your code goes here
+         if(root==null)
+            return;
+        printReverse(root.next);
+        System.out.print(root.data+" ");
+	}
+
+}
