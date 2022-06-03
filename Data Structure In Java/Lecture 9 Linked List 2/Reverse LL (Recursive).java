175af4ff8f29af7c8657156abd20a0052f324eb4
Manoj Singh <86585545+MaahiSinghGit@users.noreply.github.com>
2022-06-03 20:25:48 +0530

    Add files via upload

---
+++ Data Structure In Java/Lecture 9 Linked List 2/Reverse LL (Recursive).java
@@ -0,0 +1,22 @@
+public class Solution {
+
+	public static LinkedListNode<Integer> reverseLinkedListRec(LinkedListNode<Integer> head) {
+		//Your code goes here
+         if(head==null || head.next == null)
+        {
+            return head;
+        }
+        
+        LinkedListNode<Integer> shead = reverseLinkedListRec(head.next);
+        LinkedListNode<Integer> tail = shead;
+        while(tail.next!=null)
+        {
+            tail=tail.next;
+        }
+        tail.next=head;
+        head.next=null;
+        
+        return shead;
+	}
+
+}
